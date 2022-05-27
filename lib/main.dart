d0791d1a1bc2fb31e7bc3b526de47a700caa932a
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-27 13:22:36 +0200

    TheGorgeousLogin

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:the_gorgeous_login/pages/login_page.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TheGorgeousLogin',
+      home: LoginPage(),
+    );
+  }
+}
