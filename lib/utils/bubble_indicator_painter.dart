e2f69e426d821285d6e2aae0efc0079cd19f083f
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-30 20:10:59 +0200

    this.dxEntry = 25.0,

--- lib/utils/bubble_indicator_painter.dart
+++ lib/utils/bubble_indicator_painter.dart
@@ -6,4 +6,5 @@ import 'package:the_gorgeous_login/theme.dart';
 class BubbleIndicatorPainter extends CustomPainter {
   BubbleIndicatorPainter(
       {this.dxTarget = 125.0,
+      this.dxEntry = 25.0,
  
