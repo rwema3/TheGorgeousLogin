41a9cc4493efb5cb13caf6395e1f61a3f77e8ac9
Rwema <52289151+rwema3@users.noreply.github.com>
2022-05-30 20:13:26 +0200

    canvas.drawShadow(path, CustomTheme.loginGradientStart, 3.0, true);

--- lib/utils/bubble_indicator_painter.dart
+++ lib/utils/bubble_indicator_painter.dart
@@ -44,5 +44,6 @@ class BubbleIndicatorPainter extends CustomPainter {
         Rect.fromCircle(center: target, radius: radius), 1.5 * pi, 1 * pi);
 
     canvas.translate(size.width * pageOffset, 0.0);
+    canvas.drawShadow(path, CustomTheme.loginGradientStart, 3.0, true);
   
 }
