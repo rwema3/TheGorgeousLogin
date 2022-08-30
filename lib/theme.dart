904f7d13bf41bc6624cabe7b2231ba7c3567d723
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-08-30 16:40:39 -0700

    TDXM

--- lib/theme.dart
+++ lib/theme.dart
@@ -9,11 +9,3 @@ class CustomTheme {
   static const Color loginGradientEnd = Color(0xFFf7418c);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-
-  static const LinearGradient primaryGradient = LinearGradient(
-    colors: <Color>[loginGradientStart, loginGradientEnd],
-    stops: <double>[0.0, 1.0],
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-  );
-}
